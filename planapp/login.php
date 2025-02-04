1922886566b2cac0a233389af33a141272d5c2c1
polekkieee <mrsampolak@gmail.com>
2025-02-04 12:53:46 +0100

    add logout functionality and improve login.php structure for better session handling

--- planapp/login.php
+++ planapp/login.php
@@ -16,49 +16,60 @@ $error = false;
 </head>
 
 <body>
-    <div class="loginbody">
-        <div class="login-container">
-            <h2>Login - <strong>SweetPlans</strong></h2>
-            <form method="post">
-                <input type="email" name="_email" placeholder="Email" required>
-                <input type="password" name="_password" placeholder="Password" required>
-                <input type="submit" value="Login" name="save">
-                <a class="signupbutton" href="signup">Sign up</a>
-            </form>
+    <?php
+    if (isset($_SESSION["loggedin"])) {
+    ?>
+        <a class="signupbutton" href="logout">Log out</a>
+    <?php
+    } else {
+    ?>
+        <div class="loginbody">
+            <div class="login-container">
+                <h2>Login - <strong>SweetPlans</strong></h2>
+                <form method="post">
+                    <input type="email" name="_email" placeholder="Email" required>
+                    <input type="password" name="_password" placeholder="Password" required>
+                    <input type="submit" value="Login" name="save">
+                    <a class="signupbutton" href="signup">Sign up</a>
+                </form>
 
-            <?php
-            if (isset($_POST["save"])) {
-                $_email = $_POST["_email"];
-                $_password = $_POST["_password"];
+                <?php
+                if (isset($_POST["save"])) {
+                    $_email = $_POST["_email"];
+                    $_password = $_POST["_password"];
 
-                $sql = "SELECT * FROM users WHERE email = :email AND password = :password";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(':email', $_email);
-                $_passworde = md5($_password);
-                $stmt->bindParam(':password', $_passworde);
-                $stmt->execute();
-                $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                $result = $stmt->fetch();
-                $rows = $stmt->rowCount();
-                if ($rows == 1) {
-                    $_SESSION["loggedin"] = true;
-                    $_SESSION["email"] = $_email;
-                    $_SESSION["name"] = $result["name"];
-                    $_SESSION["userId"] = $result["id"];
-                    echo "<p class='registered'>Logged in succesfully</p>";
-                    // header("location:profile");
+                    $sql = "SELECT * FROM users WHERE email = :email AND password = :password";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bindParam(':email', $_email);
+                    $_passworde = md5($_password);
+                    $stmt->bindParam(':password', $_passworde);
+                    $stmt->execute();
+                    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                    $result = $stmt->fetch();
+                    $rows = $stmt->rowCount();
+                    if ($rows == 1) {
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["email"] = $_email;
+                        $_SESSION["name"] = $result["name"];
+                        $_SESSION["userId"] = $result["id"];
+                        echo "<p class='registered'>Logged in succesfully</p>";
+                        // header("location:profile");
 
-                } else {
-                    echo "<p class='registered'>User not found</p>";
+                    } else {
+                        echo "<p class='registered'>User not found</p>";
+                    }
                 }
-            }
-            if ($error) {
-                echo "<p class='registered'>Wrong email and/or password</p>";
-            }
-            ?>
+                if ($error) {
+                    echo "<p class='registered'>Wrong email and/or password</p>";
+                }
+                ?>
 
+            </div>
         </div>
-    </div>
+    <?php
+    }
+    ?>
+
 </body>
 
 </html>
