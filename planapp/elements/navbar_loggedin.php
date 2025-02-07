dd393992b5fa2c7758f9b6a41c4e53c5f1e968b2
polekkieee <mrsampolak@gmail.com>
2025-02-08 00:52:43 +0100

    add navbar component for logged-in users and refactor index and mycalendar pages to include it

---
+++ planapp/elements/navbar_loggedin.php
@@ -0,0 +1,24 @@
+<nav class="static">
+    <div class="width-33">
+        <img class="logoimg" id="logoimg" src="img/logo.svg" alt="SweetPlans Logo">
+    </div>
+    <div class="width-33">
+        <h1><strong>SweetPlans</strong></h1>
+    </div>
+    <div class="account-buttons">
+        <div><a class="loginbutton" href="mycalendar">MyCalendar</a></div>
+        <div><a class="signupbutton" href="logout">Log Out</a></div>
+    </div>
+    <div class="burgermenu nav-container">
+        <input class="checkbox" type="checkbox" name="" id="" />
+        <div class="hamburger-lines">
+            <span class="line line1"></span>
+            <span class="line line2"></span>
+            <span class="line line3"></span>
+        </div>
+        <div class="menu-items">
+            <li><a class="loginbutton" href="mycalendar">MyCalendar</a></li>
+            <li><a class="signupbutton" href="logout">Log Out</a></li>
+        </div>
+    </div>
+</nav>
