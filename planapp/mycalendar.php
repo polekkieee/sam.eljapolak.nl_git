17c966175e3b2eb261c3584abe6c98398c7b3535
polekkieee <mrsampolak@gmail.com>
2025-02-08 01:13:41 +0100

    remove static navbar implementation from mycalendar.php

--- planapp/mycalendar.php
+++ planapp/mycalendar.php
@@ -31,32 +31,6 @@ if ($_SESSION["loggedin"] !== true) {
   <title>My Calendar - <?php echo $_SESSION["name"] ?></title>
 </head>
 <body>
-  <header>
-    <nav class="static">
-      <div class="width-33">
-        <img class="logoimg small" id="logoimg" src="img/logo.svg" alt="SweetPlans Logo">
-      </div>
-      <div class="width-33">
-        <h1><strong>SweetPlans</strong></h1>
-      </div>
-      <div class="account-buttons">
-        <div><a class="loginbutton" href="mycalendar">MyCalendar</a></div>
-        <div><a class="signupbutton" href="logout">Log Out</a></div>
-      </div>
-      <div class="burgermenu nav-container">
-        <input class="checkbox" type="checkbox" name="" id="" />
-        <div class="hamburger-lines">
-          <span class="line line1"></span>
-          <span class="line line2"></span>
-          <span class="line line3"></span>
-        </div>
-        <div class="menu-items">
-          <li><a class="loginbutton" href="mycalendar">MyCalendar</a></li>
-          <li><a class="signupbutton" href="logout">Log Out</a></li>
-        </div>
-      </div>
-    </nav>
-  </header>
   <div class="container">
     <div class="left">
       <div class="calendar">
