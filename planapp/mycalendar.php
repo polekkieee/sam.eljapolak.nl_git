0586723476859898f4c6df4695307bc23f895bf9
polekkieee <mrsampolak@gmail.com>
2025-02-09 12:55:01 +0100

    add navigation header and burger menu to mycalendar.php; update appstyle.css for styling

--- planapp/mycalendar.php
+++ planapp/mycalendar.php
@@ -30,7 +30,31 @@ if ($_SESSION["loggedin"] !== true) {
   <link rel="icon" href="/planapp/favicon.ico">
   <title>My Calendar - <?php echo $_SESSION["name"] ?></title>
 </head>
+
 <body>
+  <header>
+    <nav class="static">
+      <div class="width-33">
+        <img class="logoimg" id="logoimg" src="img/logo.svg" alt="SweetPlans Logo">
+      </div>
+      <div class="width-33">
+        <h1><strong>SweetPlans</strong></h1>
+      </div>
+      <div class="burgermenu nav-container">
+        <input class="checkbox" type="checkbox" name="" id="" />
+        <div class="hamburger-lines">
+          <span class="line line1"></span>
+          <span class="line line2"></span>
+          <span class="line line3"></span>
+        </div>
+        <div class="menu-items">
+          <li><a class="loginbutton" href="mycalendar">MyCalendar</a></li>
+          <li><a class="signupbutton" href="logout">Log Out</a></li>
+        </div>
+      </div>
+    </nav>
+  </header>
+  
   <div class="container">
     <div class="left">
       <div class="calendar">
